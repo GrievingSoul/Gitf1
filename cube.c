7d503a06abe30e3b0139199b1f804f494279395c
GrievingSoul <asgar.pasha@s.amity.edu>
2025-03-05 21:34:56 +0530

    test file

---
+++ cube.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value: ");
+    scanf("%d",&a);
+    printf("the cube of the given number is %d: ",(a*a*a));
+    return 0;
+}
+
